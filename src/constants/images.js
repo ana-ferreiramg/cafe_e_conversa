c348882e2d68498a140a682ddc121cfce3558416
Ana Ferreira <anapaulaferreira0309@gmail.com>
2022-11-02 15:21:34 -0300

    feat: create constants folder

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+import logo from '../assets/logo.png';
+import banner from '../assets/banner.png';
+import delivery from '../assets/delivery.png';
+import orderFood from '../assets/order_food.png';
+import waiters from '../assets/waiters.png';
+
+export default {
+  logo,
+  banner,
+  delivery,
+  orderFood,
+  waiters,
+};
