c61000060c6861c58230e5413bef37adc2146861
Ana Ferreira <anapaulaferreira0309@gmail.com>
2022-11-02 16:38:19 -0300

    feat: add Navbar and Header components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Navbar, Header } from './components';
 
-const App = () => <div id="home"></div>;
+const App = () => (
+  <div id="home">
+    <Navbar />
+    <Header />
+  </div>
+);
 
 export default App;
